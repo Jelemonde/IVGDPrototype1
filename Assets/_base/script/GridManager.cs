da736f80939b37c1a7d21cd0f2b81ab58fd69a53
Jeremie Lemonde <jeremielemonde@gmail.com>
2018-09-18 21:10:07 -0400

    Added TileInfo class, 2d array, max positions and register/unregister methods.

--- Assets/_base/script/GridManager.cs
+++ Assets/_base/script/GridManager.cs
@@ -2,15 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum RegionType
+{
+    Unknown,
+    Fire,
+    Water,
+    Earth,
+    Air
+}
+
+public class TileInfo
+{
+    public int posX;
+    public int posY;
+    public RegionType element;
+    public GameObject inhabitant;
+}
+
 public class GridManager : MonoBehaviour {
 
+    public int sizeX = 20;
+    public int sizeY = 20;
+
+    public float tileSize = 1;
+
+    private TileInfo[,] grid;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+        grid = new TileInfo[sizeX, sizeY];
 	}
+
+    public bool RegisterGameObject(GameObject target, Vector2Int gridPos)
+    {
+        TileInfo currentGrid = grid[gridPos.x, gridPos.y];
+        if (currentGrid.inhabitant != null)
+            return false;
+
+        currentGrid.inhabitant = target;
+        return true;
+    }
+
+    public bool UnregisterGameObject(GameObject target, Vector2Int gridPos)
+    {
+        TileInfo currentGrid = grid[gridPos.x, gridPos.y];
+        if (currentGrid.inhabitant != target)
+            return false;
+
+        currentGrid.inhabitant = null;
+        return true;
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	
 }
